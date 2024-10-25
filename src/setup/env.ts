06f2209724c322fc084a19e6706250a907f47cf7
Vamsi-QA <vamsi.mandapati@digital.justice.gov.uk>
2024-10-25 16:53:37 +0100

    Update circle-ci context

--- src/setup/env.ts
+++ src/setup/env.ts
@@ -5,4 +5,9 @@ export default class ENV {
     public static readonly USER_THREE = process.env.USER_THREE
     public static readonly USER_FOUR = process.env.USER_FOUR
     public static readonly PASSWORD = process.env.PASSWORD
+
+    public static readonly HMPPS_AUTH_URL = process.env.HMPPS_AUTH_URL
+    public static readonly TEST_HELPER_API_URL = process.env.TEST_HELPER_API_URL
+    public static readonly TESTING_CLIENT_ID = process.env.TESTING_CLIENT_ID
+    public static readonly TESTING_CLIENT_SECRET = process.env.TESTING_CLIENT_SECRET
 }
